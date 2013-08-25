a0ecd888398eadbb7267475e0b79b87404686a8e
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2013-08-25 22:37:51 +0200

    Moved drawing logic into the shape

--- js/shape.js
+++ js/shape.js
@@ -1,19 +1,27 @@
 (function() {
-  function Shape(program, vertices, itemSize, numItems) {
-    this.program = program,
-    this.vertices = vertices,
-    this.itemSize = itemSize,
-    this.numItems = numItems;
+  function Shape(program, vertices, position, drawMethod) {
+    this.program = program;
+    this.context = program.context;
+    this.vertices = vertices;
+    this.itemSize = 3;
+    this.numItems = vertices.length/this.itemSize;
+    this.position = position;
+    this.drawMethod = drawMethod;
     this.init();
   };
 
   Shape.prototype = {
     init: function() {
+      this.modelViewMatrix = mat4.create();
       this.buffer = this.program.createBuffer(this.vertices);
     },
 
     draw: function() {
       this.context.bindBuffer(this.context.ARRAY_BUFFER, this.buffer);
+      this.context.vertexAttribPointer(this.program.getAttribute('aVertexPosition'), this.itemSize, this.context.FLOAT, false, 0, 0);
+      mat4.translate(this.modelViewMatrix, this.modelViewMatrix, this.position);
+      this.context.uniformMatrix4fv(this.program.getUniform('uMVMatrix'), false, this.modelViewMatrix);
+      this.context.drawArrays(this.drawMethod, 0, this.numItems);
     },
   };
 
--- lesson_1/js/lesson1.js
+++ lesson_1/js/lesson1.js
@@ -26,7 +26,7 @@
         -1.0, -1.0, 0.0,
          1.0, -1.0, 0.0,
       ];
-      this.triangle = new Shape(this.glProgram, vertices, 3, 3);
+      this.triangle = new Shape(this.glProgram, vertices, [-1.5, 0.0, -7.0], this.context.TRIANGLES);
       this.triangle.init();
 
       vertices = [
@@ -35,7 +35,7 @@
          1.0, -1.0, 0.0,
         -1.0, -1.0, 0.0,
       ];
-      this.square = new Shape(this.glProgram, vertices, 3, 4);
+      this.square = new Shape(this.glProgram, vertices, [1.5, 0.0, -7.0], this.context.TRIANGLE_STRIP);
       this.square.init();
     },
 
@@ -45,31 +45,11 @@
 
       mat4.perspective(this.projectionMatrix, 45, this.width / this.height, 0.1, 100.0);
 
-      mat4.identity(this.modelViewMatrix);
-
-      var vertexPositionAttribute = this.glProgram.getAttribute('aVertexPosition');
-      mat4.translate(this.modelViewMatrix, this.modelViewMatrix, [-1.5, 0.0, -7.0]);
-
-      this.context.bindBuffer(this.context.ARRAY_BUFFER, this.triangle.buffer);
-      this.context.vertexAttribPointer(vertexPositionAttribute, this.triangle.itemSize, this.context.FLOAT, false, 0, 0);
-
-      this.setMatrixUniforms();
-
-      this.context.drawArrays(this.context.TRIANGLES, 0, this.triangle.numItems);
-
-      mat4.translate(this.modelViewMatrix, this.modelViewMatrix, [3.0, 0.0, 0.0]);
-
-      this.context.bindBuffer(this.context.ARRAY_BUFFER, this.square.buffer);
-      this.context.vertexAttribPointer(vertexPositionAttribute, this.square.itemSize, this.context.FLOAT, false, 0, 0);
-
-      this.setMatrixUniforms();
-
-      this.context.drawArrays(this.context.TRIANGLE_STRIP, 0, this.square.numItems);
-    },
-
-    setMatrixUniforms: function() {
       this.context.uniformMatrix4fv(this.glProgram.getUniform('uPMatrix'), false, this.projectionMatrix);
-      this.context.uniformMatrix4fv(this.glProgram.getUniform('uMVMatrix'), false, this.modelViewMatrix);
+      this.triangle.draw();
+
+      this.context.uniformMatrix4fv(this.glProgram.getUniform('uPMatrix'), false, this.projectionMatrix);
+      this.square.draw();
     },
   };
 
